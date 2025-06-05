371a1bb7f1ea11fa6703d069e56a8ea1edaaf583
Will-waz <willianalves4998@gmail.com>
2025-06-05 20:24:13 -0300

    Add files via upload

--- php/cadastro.php
+++ php/cadastro.php
@@ -1,56 +1,40 @@
 <?php
-
 require 'conexao.php';
-
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-
     $nome = trim($_POST['nome']);
     $email = trim($_POST['email']);
     $senha = $_POST['senha'];
     $confirma_senha = $_POST['confirma_senha'];
-
-
     if ($senha !== $confirma_senha) {
         header("Location: index.html?erro=senhas_diferentes");
         exit;
     }
-
-
     $sqlCheck = "SELECT id FROM clientes WHERE email = :email";
     $stmtCheck = $pdo->prepare($sqlCheck);
     $stmtCheck->bindParam(':email', $email);
     $stmtCheck->execute();
-
     if ($stmtCheck->rowCount() > 0) {
         header("Location: ../htmls/index.php");
         $emailvalidacao = true;
         exit;
     }
-
-
     $data_cadastro = date('Y-m-d H:i:s');
-
-
     $sql = "INSERT INTO Clientes (nome, email, senha, data_cadastro) VALUES (:nome, :email, :senha, :data_cadastro)";
     $stmt = $pdo->prepare($sql);
     $stmt->bindParam(':nome', $nome);
-    $stmt->bindParam(e':email', $email);
+    $stmt->bindParam(':email', $email);
     $stmt->bindParam(':senha', $senha);
     $stmt->bindParam(':data_cadastro', $data_cadastro);
-
     if ($stmt->execute()) {
         header("Location: ../htmls/login.html?cadastro=sucesso");
         exit;
-
     } else {
- 
         header("Location: ../htmls/index.html?erro=erro_banco");
         exit;
     }
 } else {
-
     header("Location: ../htmls/index.html");
     exit;
 }
-?>
+
 
--- php/conexao.php
+++ php/conexao.php
@@ -1,12 +1,10 @@
 <?php
 $host = 'srv1549.hstgr.io';
-$db = 'u210018739_crud_php';
+$db = 'biblioteca';
 $user = 'u210018739_isaac';
 $password = '12345678Is$';
 try {
-    $pdo = new PDO("mysql:host=$host; dbname=$db; charset=utf8", $user, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $password);
 } catch (PDOException $e) {
-    echo "Erro na conexão: " . $e->getMessage();
-    exit;
+    exit("Erro na conexão com o banco de dados.");
 }
-?>
