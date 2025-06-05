08aa3fd8e50f8a2ac7e1cb0f095d64efa9afafab
Felipekbarrios <barriosfelipek@gmail.com>
2025-06-05 19:08:08 -0300

    admin.php

--- php/admin.php
+++ php/admin.php
@@ -22,19 +22,17 @@ $livros = $stmt->fetchAll();
 </head>
 <body>
 
-<h1>Área Administrativa - Lista de Filmes</h1>
-<a href="">Cadastrar novo filme</a> | <a href="">Sair</a>
+<h1>Área Administrativa - Lista de Livros</h1>
+<a href="create.php">Cadastrar novo livro</a> | <a href="logout.php">Sair</a>
 <hr>
 
 <?php foreach ($livros as $livro): ?>
     <div>
-        <h3><?php echo htmlspecialchars($livro['nome']); ?></h3>
-        <p><strong>Gênero:</strong> <?php echo htmlspecialchars($livro['genero']); ?></p>
-        <p><strong>Ano:</strong> <?php echo htmlspecialchars($livro['ano']); ?></p>
-        <p><strong>Sinopse:</strong> <?php echo htmlspecialchars($livro['sinopse']); ?></p>
-        <img src="<?php echo htmlspecialchars($livro['imagem']); ?>" alt="Imagem do filme" width="200">
+        <h3><?php echo htmlspecialchars($livro['titulo']); ?></h3>
+        <p><strong>Autor:</strong> <?php echo htmlspecialchars($livro['autor']); ?></p>
+        <p><strong>Preço:</strong> <?php echo htmlspecialchars($livro['preco']); ?></p>
+        <p><strong>Em Estoque:</strong> <?php echo htmlspecialchars($livro['estoque']); ?></p>
         
-        <!-- Links para editar e deletar -->
         <br><br>
         <a href="edit.php?id=<?php echo $livro['id']; ?>">Editar</a> |
         <a href="delete.php?id=<?php echo $livro['id']; ?>" onclick="return confirmarExclusao();">Deletar</a>
