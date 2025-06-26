0e17ff1250877e603cca952e265664f63095e8c2
gustafrias <gustavo.frias0608@gmail.com>
2025-06-25 21:33:22 -0300

    .

--- php/functions/pgPesquisa.php
+++ php/functions/pgPesquisa.php
@@ -30,10 +30,28 @@
 
             <nav class="main-nav">
                 <ul>
-                    <li><a href="../../index.php">Home</a></li>
-                    <li><a href="../../htmls/AboutUs.php">sobre nós</a></li>
-                    <li><a href="../../htmls/cadastro.html">Cadastrar-se</a></li>
-                    <li><a href="../../htmls/login.html">login</a></li>
+                  <ul id="nav-list">
+                    <?php if (!isset($_SESSION['usuario']) && !isset($_SESSION['admin'])): ?>            
+                         <li><a href="../../index.php">Home</a></li>
+                         <li><a href="../../htmls/AboutUs.php">Sobre nós</a></li>
+                        <li><a href="../../htmls/cadastro.html">Cadastrar-se</a></li>
+                        <li><a href="../../htmls/login.html">Login</a></li>
+                    <?php else: ?>
+                        <li>
+                            <li><a href="../../index.php">Home</a></li>
+                            <li><a href="../../htmls/AboutUs.php">Sobre nós</a></li>
+                            <img src="../../img/iconLogin.png" alt="" id="icon-login"
+                                style="width: 50px; height: auto; margin-top: -13px; margin-right: -30px;">
+                        </li>
+                        <li>
+                            <?php if (isset($_SESSION['admin'])): ?>
+                                <li><a href="../login&cadastro/admin.php">Administrador</a></li>
+                            <?php elseif (isset($_SESSION['usuario'])): ?>
+                                <?= htmlspecialchars($_SESSION['usuario']) ?>
+                            <?php endif; ?>
+                        </li>
+                        <li><a href="../login&cadastro/logout.php">Sair</a></li>
+                    <?php endif; ?>
                 </ul>
             </nav>
         </div>
